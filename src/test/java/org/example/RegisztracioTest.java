0acab961bdad35fae1a0e53985bc70b9adf2ecde
Varga Andrea <magroandi28@gmail.com>
2026-09-14 13:51:31 +0200

    Update RegisztracioTest.java

--- src/test/java/org/example/RegisztracioTest.java
+++ src/test/java/org/example/RegisztracioTest.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import java.time.Duration;
 import org.junit.jupiter.api.DisplayName;
+
 public class RegisztracioTest {
 
     private WebDriver driver;
